4b472f10eb242fc92d6ab87431ccff0f402289e7
Admin <15243451188@163.com>
2025-02-19 17:15:59 +0800

    优化-ScreenRecordingService 尽量优化成工具类形式+1

--- app/src/main/java/mm/fanshanjia/aitest/MainActivity.kt
+++ app/src/main/java/mm/fanshanjia/aitest/MainActivity.kt
@@ -1,33 +1,15 @@
 package mm.fanshanjia.aitest
-
-import android.Manifest
-import android.app.AlertDialog
 import android.content.Intent
 import android.media.projection.MediaProjectionManager
-import android.os.Build
 import android.os.Bundle
-import android.os.Environment
-import android.util.Log
 import android.webkit.WebView
 import android.widget.Button
-import android.widget.Toast
 import androidx.activity.ComponentActivity
-import androidx.activity.result.contract.ActivityResultContracts
+
 
 class MainActivity : ComponentActivity() {
     private lateinit var mediaProjectionManager: MediaProjectionManager
     private var isRecording = false
-    private var resultCode: Int = 0
-    private var resultData: Intent? = null
-
-    private val startForResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-        if (result.resultCode == RESULT_OK) {
-            resultCode = result.resultCode
-            resultData = result.data
-            startScreenRecordingService()
-        }
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -39,27 +21,6 @@ class MainActivity : ComponentActivity() {
             } else {
                 ScreenRecordingService.checkPermissionsAndStartRecording(this)
             }
-        }
-    }
-
-
-
-    private fun startScreenRecordingService() {
-        try {
-            val serviceIntent = Intent(this, ScreenRecordingService::class.java).apply {
-                putExtra("resultCode", resultCode)
-                putExtra("resultData", resultData)
-            }
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                startForegroundService(serviceIntent)
-            } else {
-                startService(serviceIntent)
-            }
-            isRecording = true
-            findViewById<Button>(R.id.record_button).text = "停止"
-        } catch (e: Exception) {
-            Log.e("MainActivity", "启动服务失败: ${e.message}", e)
-            Toast.makeText(this, "启动录制服务失败: ${e.message}", Toast.LENGTH_LONG).show()
         }
     }
 
